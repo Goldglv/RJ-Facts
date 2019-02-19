4cdc27f8245ecc9980b0784c1a2ba96a2a096d67
Goldglv <goldglv@hotmail.com>
2019-02-19 11:17:15 -0500

    Update facts_en_US.py

--- lambda/py/data/facts_en_US.py
+++ lambda/py/data/facts_en_US.py
@@ -1,21 +1,17 @@
 data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+  "One of Riya's names for Jaina is Bob.",
+  "Jaina's friend Julianna calls Riya poop.",
+  'Riya tells everyone that Nugget is married to Punjeetha.',
+  "One day when Riya's friends car ran out of gas, the brother hooked up a hose to his sister's butt.",
+  'There is a rumor of a bro-over happeneing soon.',
+  'Jaina has a secret weapon call the snap twist pinch.',
+  'Mommy farted.....a lot.',
+  'Nugget and Dusk fart all the time, completely stinking up the first floor of the house, I think they need to see a fart specialist.',
+  "Nugget has a bad behaviour chart because he's bad.",
 ]
 
-SKILL_NAME = "American Space Facts"
+SKILL_NAME = "Riya and Jaina Facts"
 GET_FACT_MESSAGE = "Here's your fact: "
-HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?"
+HELP_MESSAGE = "You can say tell me a Riya and Jaina fact, or, you can say exit... What can I help you with?"
 HELP_REPROMPT = "What can I help you with?"
 STOP_MESSAGE = "Goodbye!"
